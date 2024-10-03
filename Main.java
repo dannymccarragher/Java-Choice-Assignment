8ed37b7fdf781c8883b2b989f5ff2f747e9559bf
Danny McCarragher <dannymccarragher@gmail.com>
2024-10-03 16:33:39 -0700

    Add Age Difference Method

--- Main.java
+++ Main.java
@@ -1,12 +1,11 @@
 
-
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        List<Dog> dogList = new ArrayList<>();  
+        List<Dog> dogList = new ArrayList<>();
         Map<String, Dog> dogMap = new HashMap<>();
 
         // Create a Dog object
@@ -17,13 +16,20 @@ public class Main {
         dogList.add(myDog);
         dogList.add(myDog2);
 
-        //Add myDog to a map 
+        // Add myDog to a map
         dogMap.put(myDog.name, myDog);
         dogMap.put(myDog2.name, myDog2);
 
-        System.out.println(dogList);
+        //System.out.println(dogList);
 
-        System.out.println(dogMap);
+        //System.out.println(dogMap);
+
+        int ageDifference = calculateAgeDifference(myDog, myDog2);
+        System.out.println("The age difference between " + myDog.name + " and " + myDog2.name + " is: " + ageDifference);
+    }
+
+    public static int calculateAgeDifference(Dog myDog, Dog myDog2) {
+        return myDog.getAge() - myDog2.getAge();
     }
 
 }
