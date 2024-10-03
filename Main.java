01542734516280281f24b54442ec116440e4730f
dannymccarragher <127553169+dannymccarragher@users.noreply.github.com>
2024-10-03 12:22:24 -0700

    Push

---
+++ Main.java
@@ -0,0 +1,39 @@
+
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Dog> dogList = new ArrayList<>();  
+        Map<String, Dog> dogMap = new HashMap<>();
+
+        // Create a Dog object
+        Dog myDog = new Dog("Maggie", "Golden Doodle", 12);
+        Dog myDog2 = new Dog("Pumpkin", "Black Lab", 3);
+
+        // Add myDog to the list
+        dogList.add(myDog);
+        dogList.add(myDog2);
+
+        //Add myDog to a map 
+        dogMap.put(myDog.name, myDog);
+        dogMap.put(myDog2.name, myDog2);
+
+    
+
+        // Print the list of dogs
+        System.out.println(dogList);
+        // Print the map of dogs
+        System.out.println(dogMap);
+        //calcuate ageDifference between the 2 dog objects
+        int ageDifference = calculateAgeDifference(myDog, myDog2);
+        System.out.println("The age difference between " + myDog.name +  " and " + myDog2.name + " is: " + ageDifference);
+    }
+
+    public static int calculateAgeDifference(Dog myDog, Dog myDog2){
+    return myDog.getAge() - myDog2.getAge();
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+
+**Java Dog Map and List Program**
+
+This Java program creates a list and a map of Dog objects, then calculates the age difference 
+between two dogs using a custom method.
+
+Within this assignment, I covered adding objects to 
+Maps and Lists.
